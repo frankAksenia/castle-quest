06eadf6da4943f823540f6508983c0c3d1a1e331
Ksenia Frank <a123456@unet.univie.ac.at>
2023-05-23 23:11:36 +0200

    Think: create controller for player registration

---
+++ Source/Teilaufgabe 3 - Server/src/main/java/server/controller/PlayerRegistrationController.java
@@ -0,0 +1,9 @@
+package server.controller;
+
+/*
+ * Controller for processing player registration requests.
+ * Verifies received data and generates appropriate response.
+ */
+public class PlayerRegistrationController {
+
+}
