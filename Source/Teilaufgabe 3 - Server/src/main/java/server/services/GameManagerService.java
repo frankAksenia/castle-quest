8df21f937f46789254957c9884c1e93f88e9776a
Ksenia Frank <a123456@unet.univie.ac.at>
2023-05-23 23:36:32 +0200

    Create game repository class and move array with games there

--- Source/Teilaufgabe 3 - Server/src/main/java/server/services/GameManagerService.java
+++ Source/Teilaufgabe 3 - Server/src/main/java/server/services/GameManagerService.java
@@ -1,24 +1,24 @@
 package server.services;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import server.model.GameData;
 import server.model.GameId;
+import server.model.GameRepository;
 
 @Service
 public class GameManagerService {
 	
-
-	/* Using LinkedHashMap to preserve order of added elements
-	 * to know which games are the oldest and can be removed if Server is overloaded
-	 * */
-	private Map<GameId, GameData> runningGames = new LinkedHashMap<>();
+	private GameRepository gameRepository;
+	
+	public GameManagerService() {
+		this.gameRepository = new GameRepository();
+	}
 	
 	public Map<GameId,GameData> getAllRunningGames() {
-		return this.runningGames;
+		return this.gameRepository.getAllRunningGames();
 	}
 	
 	public GameData getRunningGameById(GameId gameId) {
