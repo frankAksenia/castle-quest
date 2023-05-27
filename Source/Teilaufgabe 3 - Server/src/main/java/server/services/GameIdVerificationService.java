4a484cc3d77ddc2d9e9065b2fa89fdc7006c25ea
Ksenia Frank <a123456@unet.univie.ac.at>
2023-05-27 14:52:48 +0200

    Think: add classes for id verification services

---
+++ Source/Teilaufgabe 3 - Server/src/main/java/server/services/GameIdVerificationService.java
@@ -0,0 +1,12 @@
+package server.services;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * Service used by different controllers to verify if the received GameId is correct.
+ * Throws exceptions if rules are violated.
+ */
+@Service
+public class GameIdVerificationService {
+
+}
---
+++ Source/Teilaufgabe 3 - Server/src/main/java/server/services/PlayerIdVerificationSerivce.java
@@ -0,0 +1,5 @@
+package server.services;
+
+public class PlayerIdVerificationSerivce {
+
+}
